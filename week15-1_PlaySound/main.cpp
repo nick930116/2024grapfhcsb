6f889034dfb5b27925407462108c96abe43a8273
nick930116 <112918769+nick930116@users.noreply.github.com>
2024-05-27 17:05:27 +0800

    week15

---
+++ week15-1_PlaySound/main.cpp
@@ -0,0 +1,14 @@
+#include<windows.h>
+
+#include <GL/glut.h>
+
+int main()
+{
+    ///PlaySound("mykbeat.wav", NULL, SND_SYNC);
+    PlaySound("do.wav", NULL, SND_SYNC);
+    PlaySound("re.wav", NULL, SND_SYNC);
+    PlaySound("mi.wav", NULL, SND_SYNC);
+    PlaySound("fa.wav", NULL, SND_SYNC);
+    PlaySound("so.wav", NULL, SND_SYNC);
+
+}
